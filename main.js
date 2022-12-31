76836b3e02882c6fc29d0005d000178b01254b10
AltinDurakuu <altinduraku404@gmail.com>
2022-12-31 16:49:01 +0100

    hero banner slider and some changes on chat live now button

--- main.js
+++ main.js
@@ -8,3 +8,29 @@
     zoom: 20
   });
 }
+// Get the slider container/dots container and slides/dots
+const sliderContainer = document.querySelector('.hero-banner-slider-container');
+const dotsContainer = document.querySelector('.hero-slider-dots');
+const slides = Array.from(sliderContainer.querySelectorAll('.hero-banner-slider'));
+const dots = Array.from(dotsContainer.querySelectorAll('.dot'));
+
+// Set the current slide/dot index to 0
+let currentSlideIndex = 0;
+
+// Add the "active" class to the first slide/dot
+slides[currentSlideIndex].classList.add('hero-banner-slider--active');
+dots[currentSlideIndex].classList.add('hero-slider-dots__active');
+
+// Set the interval for the automatic slideshow/dots
+const interval = setInterval(() => {
+  // Remove the "active" class from the current slide/dot
+  slides[currentSlideIndex].classList.remove('hero-banner-slider--active');
+  dots[currentSlideIndex].classList.remove('hero-slider-dots__active');
+
+  // Increment the current slide/dot index, or reset to 0 if we've reached the end
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+
+  // Add the "active" class to the next slide
+  slides[currentSlideIndex].classList.add('hero-banner-slider--active');
+  dots[currentSlideIndex].classList.add('hero-slider-dots__active');
+}, 3000);
