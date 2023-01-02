8da0bb30ff0bc51f99545f5e1d3bdd7f077a41ef
Erdin-Osmanii <117120028+Erdin-Osmanii@users.noreply.github.com>
2023-01-02 21:18:39 +0100

    Some general fixes

--- js/latest-blog.js
+++ js/latest-blog.js
@@ -3,6 +3,7 @@ console.log(mBlogContairer);
 const mBlogs = Array.from(mBlogContairer.querySelectorAll('.main-blog'));
 
 function showBlog(n) {
-    mBlogs.forEach(mBlog => mBlog.classList.remove('.main-blog--active'));
-    mBlogs[n].classList.add('.main-blog--active');
-  }
+  console.log(n);
+    mBlogs.forEach(mBlog => mBlog.classList.remove('main-blog--active'));
+    mBlogs[n].classList.add('main-blog--active');
+}
