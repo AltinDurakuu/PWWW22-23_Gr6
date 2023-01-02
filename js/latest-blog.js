1013eb9f5849304c67856b74bb2d3ea371fbebbc
Erdin-Osmanii <117120028+Erdin-Osmanii@users.noreply.github.com>
2023-01-02 20:48:50 +0100

    Latest blog js additions

---
+++ js/latest-blog.js
@@ -0,0 +1,8 @@
+const mBlogContairer = document.querySelector('.main-blog-container');
+console.log(mBlogContairer);
+const mBlogs = Array.from(mBlogContairer.querySelectorAll('.main-blog'));
+
+function showBlog(n) {
+    mBlogs.forEach(mBlog => mBlog.classList.remove('.main-blog--active'));
+    mBlogs[n].classList.add('.main-blog--active');
+  }
