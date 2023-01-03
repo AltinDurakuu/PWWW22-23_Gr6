bded625b438ab6c8ecbd391bebb52498f52bd780
Erdin-Osmanii <117120028+Erdin-Osmanii@users.noreply.github.com>
2023-01-03 02:19:34 +0100

    Fixces in the latest blog page

--- js/latest-blog.js
+++ js/latest-blog.js
@@ -1,9 +1,18 @@
 const mBlogContairer = document.querySelector('.main-blog-container');
-console.log(mBlogContairer);
 const mBlogs = Array.from(mBlogContairer.querySelectorAll('.main-blog'));
+window.onload=showBlog(0);
+
+
 
 function showBlog(n) {
-  console.log(n);
-    mBlogs.forEach(mBlog => mBlog.classList.remove('main-blog--active'));
-    mBlogs[n].classList.add('main-blog--active');
+  try {
+    if (n>=mBlogs.length || n<0) {
+      throw new TypeError();
+    }
+  } catch (error) {
+    alert("Sorry! Article doesn't exist.");
+    return;
+  }  
+  mBlogs.forEach(mBlog => mBlog.classList.remove('main-blog--active'));
+  mBlogs[n].classList.add('main-blog--active');
 }
